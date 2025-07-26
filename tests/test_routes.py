5856471c022dd01534846c5ab9a4810b51d3edbe
Karan Maheshwari <krn.maheshwari9@gmail.com>
2025-07-26 06:05:10 -0400

    Added delete account with testing

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -166,6 +166,17 @@ class TestAccountService(TestCase):
         self.assertEqual(updated_account["name"], "Something Known")
     
     def test_update_account_not_found(self):
-        """It should not Read an Account that is not found"""
+        """It should not Update an Account that is not found"""
         resp = self.client.put(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_delete_account_not_found(self):
+        """It should not DELETE an Account that is not found"""
+        resp = self.client.delete(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
